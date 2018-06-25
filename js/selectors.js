856f597a94f89e0cb82a7f3c2f66e23b5c9f8089
Sophia <spuertas@micds.org>
2018-06-25 13:59:25 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,18 @@
 'use-strict';
 //add your code here
+//ASK ABOUT THESE
+function h1Selector() {
+  return $('h1')
+}
+function liInOlSelector() {
+  return $('ol li')
+}
+function linkSelector() {
+  return $('a','#box4','.box5')
+}
+function imageSelector() {
+  return $("[alt='cat sleeping']")
+}
+function checkboxInputSelector() {
+  return $('input[type=checkbox]')
+}
