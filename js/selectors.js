1169dda6302d98955c5342118dc977e104b12cd1
Sophia <spuertas@micds.org>
2018-06-25 13:59:52 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,17 +2,17 @@
 //add your code here
 //ASK ABOUT THESE
 function h1Selector() {
-  return $('h1')
+  return $('h1');
 }
 function liInOlSelector() {
-  return $('ol li')
+  return $('ol li');
 }
 function linkSelector() {
-  return $('a','#box4','.box5')
+  return $('a','#box4','.box5');
 }
 function imageSelector() {
-  return $("[alt='cat sleeping']")
+  return $("[alt='cat sleeping']");
 }
 function checkboxInputSelector() {
-  return $('input[type=checkbox]')
+  return $('input[type=checkbox]');
 }
